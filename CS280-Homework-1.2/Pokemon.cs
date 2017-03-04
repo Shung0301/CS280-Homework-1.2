bc0ae45374d219c327f1dcd00b359cc760c0d2d9
Shung0301 <tylertangbu@gmail.com>
2017-03-05 01:00:27 +0800

    初始化專案

---
+++ CS280-Homework-1.2/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Pokemon
+    {
+        public float SerialNumber;
+        public string PokemonName;
+    }
+}
---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.SerialNumber = 5566f;
+            pokemon.PokemonName = "小瓷怪";
+
+            Console.WriteLine("寶可夢編號:{0}, 寶可夢名稱:{1}"
+                , pokemon.SerialNumber, pokemon.PokemonName);
+        }
+    }
+}
